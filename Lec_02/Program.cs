0d1ffe77591fabb9063e4f0babefc5fd4b928264
FrankHorrigun <FrankHorrigun@mail.ru>
2023-04-06 11:48:15 +0200

    1-2 lec

---
+++ Lec_02/Program.cs
@@ -0,0 +1,39 @@
+﻿// Big O Notation
+
+// таблица умонжения от 1 до n
+
+int n = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 1; i <= n; i++)
+{
+    for (int j = 1; j <= n; j++)
+    {
+        Console.Write(i * j + "\t");
+    }
+    Console.WriteLine();
+}
+// сложность 0(n^2)
+// оптимизируем
+int[, ] matr = new int[n, n];
+for (int i = 0; i < n; i++)
+{
+    for (int j = i; j < n; j++)
+    {
+        matr[i, j] = (i + 1) * (j + 1);
+        matr[j, i] = (i + 1) * (j + 1);
+    }
+}
+ Console.WriteLine();
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < n; j++)                 
+    {                                           
+        Console.Write(matr[i, j]);
+        Console.Write("\t");
+    }
+    Console.WriteLine();
+}
+// сложность O(n^2/2)
+
+
+
---
+++ Lec_1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Diagnostics;
+
+int size = 1_000_000;
+int m = 3_000;
+
+int[] array = Enumerable.Range(1, size)
+                        .Select(item => Random.Shared.Next(10))
+                        .ToArray();
+
+// Console.WriteLine($"[{string.Join(", ", array)}]");
+
+Stopwatch sw = Stopwatch.StartNew();
+// sw.Start();
+
+int max = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    int t = 0;
+    for (int j = 0; j < array.Length - m; j++) t += array[j];
+    if (t > max) max = t;
+}
+// оптимизируем
+// int max = 0;
+// for (int j = 0; j < array.Length - m; j++) max += array[j];
+// int t = max;
+// for (int i = 0; i < array.Length; i++)
+// {
+//     t = t - array[i - 1] + array[i + (m - 1)];
+// }
+
+
+sw.Stop();
+Console.WriteLine($"time={sw.ElapsedMilliseconds}");
+Console.WriteLine(max);
