421c89d413fea7d2f82841047930d07151c63899
FrankHorrigun <FrankHorrigun@mail.ru>
2023-04-07 14:56:49 +0200

    lec_04_BubleSortClassic

---
+++ Lec_04_BubleSort/Program.cs
@@ -0,0 +1,18 @@
+﻿// BubleSort
+// classic
+int[] array = { 3, 2, 5, 6, 7, 5, 9, 2 };
+Console.WriteLine($"[{string.Join(", ", array)}]");
+
+for (int i = 0; i < array.Length; i++)
+{
+    for (int j = 0; j < array.Length-1; j++)
+    {
+        int temp = array[j];
+        if (array[j] > array[j+1])
+        {
+            array[j] = array[j+1];
+            array[j+1] = temp;
+        }
+    }
+}
+Console.WriteLine($"[{string.Join(", ", array)}]");
